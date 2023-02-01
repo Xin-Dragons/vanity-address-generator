d279fb83b95ace97f283c92263621b9b3e75a08a
Aleksandar Kolov <alex@xindragons.io>
2023-02-01 10:17:30 +0100

    added GA

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,6 @@
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import '../styles/globals.css';
+import Script from 'next/script'
 
 const theme = createTheme({
   palette: {
@@ -11,10 +12,26 @@ const theme = createTheme({
 })
 
 const App = ({ Component, pageProps }) => {
+  
   return (
+    <><Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-0FJF0WRDRK"/>
+        <Script
+          id='google-analytics'
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-0FJF0WRDRK', {
+                page_path: window.location.pathname,
+              });
+            `,
+            }}
+        />
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
-    </ThemeProvider>
+    </ThemeProvider></>
   );
 };
 
